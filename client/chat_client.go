631a2beda54291ea91f944d92df628757428ffb5
Mauro Labruna <m.labruna@shopfully.com>
2025-07-18 14:28:05 +0200

    feat: add support for configuring the server host

--- client/chat_client.go
+++ client/chat_client.go
@@ -1,34 +1,44 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "net"
-    "os"
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-    conn, err := net.Dial("tcp", "localhost:9000")
-    if err != nil {
-        fmt.Println("Connection error:", err)
-        return
-    }
-    defer conn.Close()
+	server := flag.String("host", "", "server IP/Hostname")
+	flag.Parse()
 
-    go func() {
-        reader := bufio.NewReader(conn)
-        for {
-            msg, err := reader.ReadString('\n')
-            if err != nil {
-                fmt.Println("Disconnected from server.")
-                os.Exit(0)
-            }
-            fmt.Print(msg)
-        }
-    }()
+	fmt.Println(*server)
 
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-        fmt.Fprintln(conn, scanner.Text())
-    }
+	if *server == "" {
+		fmt.Println("You must specify a server to connect")
+		return
+	}
+	conn, err := net.Dial("tcp", *server+":9000")
+	if err != nil {
+		fmt.Println("Connection error:", err)
+		return
+	}
+	defer conn.Close()
+
+	go func() {
+		reader := bufio.NewReader(conn)
+		for {
+			msg, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Disconnected from server.")
+				os.Exit(0)
+			}
+			fmt.Print(msg)
+		}
+	}()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Fprintln(conn, scanner.Text())
+	}
 }
